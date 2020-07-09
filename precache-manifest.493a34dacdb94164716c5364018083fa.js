fbc82109e7b4ebfbf3734cce2525e4908d672bba
fillopfy <abhi456tm@gmail.com>
2020-07-09 19:46:36 +0530

    Updates

---
+++ precache-manifest.493a34dacdb94164716c5364018083fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "086ffbcf2df6482f78099e88cc16e74f",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "d86d3e5921a226c36a20",
+    "url": "/todo/static/css/main.1e6c14f2.chunk.css"
+  },
+  {
+    "revision": "653081b9d37e49d0bf28",
+    "url": "/todo/static/js/2.4da34c69.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo/static/js/2.4da34c69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d86d3e5921a226c36a20",
+    "url": "/todo/static/js/main.e637a0ca.chunk.js"
+  },
+  {
+    "revision": "da7c3f7f7e5963fe74b8",
+    "url": "/todo/static/js/runtime-main.74315f96.js"
+  }
+]);
